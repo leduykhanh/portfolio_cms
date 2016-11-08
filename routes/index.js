7cf824a16d3ea4b47a9861f9a49e0b7f1fb0557b
jangkoo <le.khanh@goldenequator.com>
2016-11-08 18:09:12 +0800

    menu

--- routes/index.js
+++ routes/index.js
@@ -61,5 +61,7 @@ exports = module.exports = function (app) {
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
+	//Dota
+	app.all('/dota/leaderboards', routes.views.dota.leaderboards);
 
 };
