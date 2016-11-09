d941b6f7cad54b8d54e1d0adf71bb5c4c6c5e5a4
jangkoo <le.khanh@goldenequator.com>
2016-11-09 18:38:34 +0800

    leaderboards

---
+++ public/js/dota/leaderboard.js
@@ -0,0 +1,17 @@
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  var target = $(e.target).attr("href") ;// activated tab
+  console.log(target);
+  if ($(target).is(':empty')) {
+    $.ajax({
+      type: "GET",
+      url: "/article/",
+      error: function(data){
+        alert("There was a problem");
+      },
+      success: function(data){
+        $(target).html(data);
+      }
+  })
+ }
+})
