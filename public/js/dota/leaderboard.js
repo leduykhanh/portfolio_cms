fef9ba36955b565cc7a276692adcad149b302650
jangkoo <le.khanh@goldenequator.com>
2017-01-20 15:17:00 +0800

    adsense

--- public/js/dota/leaderboard.js
+++ public/js/dota/leaderboard.js
@@ -13,7 +13,7 @@ function loadData(target){
         
         var table = $('<table class="table table-hover text-left"></table>');
         JSON.parse(data).leaderboard.map(function(value,index){
-          table.append('<tr><td>' + value.rank + '</td><td>' + value.team_tag + '.' + value.name + '<span class="flag-icon flag-icon-'+ value.country+'"></span>' + '</td>' + '<td>' + value.solo_mmr + '</td>' + '</tr>');
+          table.append('<tr><td>' + value.rank + '</td><td>' + value.team_tag + '.' + value.name +'</td><td>' + '<span class="flag-icon flag-icon-'+ value.country+'"></span>' + '</td><td>' + '</td>' + '<td>' + value.solo_mmr + '</td>' + '</tr>');
         })
         $(target).html(table);
       }
