89a03ade424aa5222f81ed8f56764af99de1030e
jangkoo <le.khanh@goldenequator.com>
2017-02-02 13:25:35 +0800

    logo

--- public/js/home.js
+++ public/js/home.js
@@ -44,3 +44,19 @@ window.signinModalTrigger = function signinModalTrigger(e) {
 }
 
 $("[href='#modal-auth'], [data-modal='auth'], .js-auth-trigger").on('click', signinModalTrigger);
+function animateValue(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+animateValue("order-count",0,53,1.5);
+animateValue("customer-count",0,53,1.5);
