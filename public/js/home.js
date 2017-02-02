5e1618f86c3b850dbd96f1d3b768760566141ab1
jangkoo <le.khanh@goldenequator.com>
2017-02-02 13:43:26 +0800

    menu

--- public/js/home.js
+++ public/js/home.js
@@ -67,5 +67,8 @@ $(window).scroll(function() {
        animateValue("order-count",0,1532,1500);
        animateValue("customer-count",0,53,500);
    }
+   if (wS > wH){
+    $(".navbar-inverse").css({"background-color": rgba(225,225,225,0.4), "color":"black"});
+   }
 });
 
