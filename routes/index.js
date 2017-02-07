34a96d3b54eabc9d93c56e588f9e1f34799e6ca8
jangkoo <le.khanh@goldenequator.com>
2017-02-07 12:28:56 +0800

    menu

--- routes/index.js
+++ routes/index.js
@@ -46,7 +46,7 @@ exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
+	app.all('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.get('/about', routes.views.about);
 	app.all('/contact', routes.views.contact);
