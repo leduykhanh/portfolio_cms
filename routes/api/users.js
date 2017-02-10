ae8be3d73cf9a504234ea6b8a2e04c07694ac7bb
jangkoo <le.khanh@goldenequator.com>
2017-02-10 12:42:15 +0800

    user

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,12 +4,15 @@ var async = require('async'),
 var User = keystone.list('User');
 
 
-exports.get = function(req, res) {
+exports.get = function(req, res,next) {
 
 	User.model.findById(req.params.id).exec(function(err, item) {
 		
 		if (err) return res.apiError('database error', err);
-		item.populateRelated('bets[user]');
+		item.populateRelated('bets[user]', function(err) {
+    			
+  			});
+	
 		res.apiResponse({
 			item
 		});
