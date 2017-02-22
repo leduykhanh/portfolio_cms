423c9bda70bdd63fc143ab8a9c6a2ed23a498d72
jangkoo <le.khanh@goldenequator.com>
2017-02-22 16:35:33 +0800

    dota

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,12 +16,14 @@ exports.signin = function(req, res) {
     }
     
     keystone.session.signin({ email: user.email, password: req.body.password }, req, res, function(user) {
-      
+      user.populateRelated('bets[user]', function(err) {
+          
+        });
       return res.json({
         success: true,
         session: true,
         date: new Date().getTime(),
-        userId: user.id
+        user: user
       });
       
     }, function(err) {
