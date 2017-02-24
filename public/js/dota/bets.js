efd0b78512d5dc804cffb9b54cc62c2d2fa8371e
jangkoo <le.khanh@goldenequator.com>
2017-02-24 17:08:02 +0800

    dit cu may

---
+++ public/js/dota/bets.js
@@ -0,0 +1,30 @@
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  $( ".expireDate" ).each(function( index, bet ) {
+    var countDownDate = new Date(bet.attr("time")).getTime();
+  // Get todays date and time
+    
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var timeLeft = "";
+    if (days > 0) timeLeft += days + "d ";
+    if (hours > 0) timeLeft += hours + "h ";
+    if (minutes > 0) timeLeft += minutes + "m";
+    // Display the result in the element with id="demo"
+    if (distance > 0)
+      bet.html(timeLeft);
+    else
+    // If the count down is finished, write some text 
+      bet.html("past");
+});
+ 
+}, 60000);
