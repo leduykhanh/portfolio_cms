4c7711d5c01049af405d0f6c68b72aa79dd88247
jangkoo <le.khanh@goldenequator.com>
2017-05-05 16:48:42 +0800

    dcmm

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,13 @@
   ga('create', 'UA-53329462-14', 'auto');
   ga('send', 'pageview');
 
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
 $('.carousel').carousel({
     interval: 5000 //changes the speed
