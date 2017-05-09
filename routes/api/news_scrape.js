3569bdce76f951641dcb1a8da6d5e27c80815379
jangkoo <le.khanh@goldenequator.com>
2017-05-09 17:40:58 +0800

    revamp

--- routes/api/news_scrape.js
+++ routes/api/news_scrape.js
@@ -16,12 +16,32 @@ var options = {
       };
 module.exports = function(req, res){
 
-	    request(options, function (error, response, body) {
+	request(options, function (error, response, body) {
 
-                 if (error) {
-                   console.log("Authorize Error: ",error,response.statusCode);
+	         if (error) {
+	           console.log("Authorize Error: ",error,response.statusCode);
 
-                 }
-                res.send(JSON.parse(body));
+	         }
+         var  articles = JSON.parse(body).articles;
+         articles.map(function(ac){
+         	var item = new Post.model();
+         	var data = {
+         		"title": ac.title,
+				"author": "57bfe0a1d59fbbb36d515005",
+				"categories": [
+				  "5855250a89b5c877541fc1ea"
+				],
+				"content": {
+				"brief": ac.description,
+				"extended": "<p>"+ac.description+"</p>" + "<p >Read more at <a href='"+ac.url+"'>"+ac.url+"</a></p>"
+				},
+				"state": "published"
+         	};
+         		item.getUpdateHandler(req).process(data, function(err) {
+		
+					if (err) return res.apiError('error', err);
+				});
+         });
+         res.send(JSON.parse(body));
             });
 }
